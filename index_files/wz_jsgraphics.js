0e1d99e74e9a75f61593fc439856e582f5101356
Frederik Triest <frederik_triest@hotmail.com>
2015-03-24 12:55:34 +0100

    Revert "Initial Commit"
    This reverts commit a3ad515e17836d68f08bce13a5f321f16271ae01.

--- index_files/wz_jsgraphics.js
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL /preview/a7a/a7a8f2a8-d152-11e4-b21a-002590d31986/a7a8f2a8-d152-11e4-b21a-002590d31986_id_CVFrederikTriest_eng_files/wz_jsgraphics.js was not found on this server.</p>
-<hr>
-<address>Apache/2.2.15 (CentOS) Server at www.pdf.investintech.com Port 80</address>
-</body></html>
